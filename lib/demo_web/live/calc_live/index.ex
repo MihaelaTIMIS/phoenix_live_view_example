19e01c85067c5afbd4abf474bb4d05b17ccff9f0
Scott Meade <scott@synapsoftware.com>
2019-03-17 11:43:00 -0600

    Initial attempt at supporting keyboard events

--- lib/demo_web/live/calc_live/index.ex
+++ lib/demo_web/live/calc_live/index.ex
@@ -9,26 +9,21 @@ defmodule DemoWeb.CalcLive.Index do
   def render(assigns), do: CalcView.render("index.html", assigns)
 
   # Digit clicks
-  def handle_event("digit", digit, socket = %{assigns: %{clear_display_on_next_digit: true}} ) do
-    {:noreply, assign(socket, display: digit, clear_display_on_next_digit: false)}
-  end
   def handle_event("digit", digit, socket) do
-    {:noreply, assign(socket, display: "#{socket.assigns.display}#{digit}")}
+    update_display(digit, socket)
   end
 
   # Number key presses
-  def handle_event("keyup", key, socket) when (key >= "0" and key <= "9") or key == "." do
-    handle_event("digit", key, socket)
+  def handle_event("keypress", key, socket) when (key >= "0" and key <= "9") or key == "." do
+    update_display(key, socket )
   end
-  def handle_event("keyup", key, socket) when key in ["+","-","/","*"] do
+  def handle_event("keypress", key, socket) when key in ["+","-","/","*"] do
     handle_event("operator", key, socket)
   end
-  def handle_event("keyup", key, socket) when key = "=" do
+  def handle_event("keypress", key, socket) when key == "=" do
     handle_event("solve", key, socket)
   end
-  def handle_event("keyup", _key, socket) do
-    {:noreply, socket}
-  end
+  def handle_event("keypress", _key, socket), do: {:noreply, socket}
 
   # Operator clicks (+,-,*,/):
   # when there is no pending operator. Whatever is on the display becomes the current value.
@@ -52,40 +47,40 @@ defmodule DemoWeb.CalcLive.Index do
 
   # Utility buttons
   def handle_event("square_root", _, socket) do
-    {:noreply, assign(socket, display: :math.sqrt(display_as_float(socket)))}
+    {:noreply, assign(socket, display: :math.sqrt(display_to_float(socket)))}
+  end
+
+  def handle_event("posneg", _, socket) do
+    {:noreply, assign(socket, display: display_to_float(socket) * -1)}
   end
 
   def handle_event("reset", _, socket) do
     {:noreply, reset_state(socket, socket.assigns.memory)}
   end
 
-  def handle_event("posneg", _, socket) do
-    {:noreply, assign(socket, display: display_as_float(socket) * -1)}
-  end
-
   # Memory events
   def handle_event("memory", "clear", socket) do
-    {:noreply, assign(socket, memory: 0, clear_display_on_next_digit: true )}
+    update_memory(socket, 0)
+  end
+
+  def handle_event("memory", "store", socket) do
+    update_memory(socket, display_to_float(socket))
+  end
+
+  def handle_event("memory", "add", socket) do
+    update_memory(socket, socket.assigns.memory + display_to_float(socket))
+  end
+
+  def handle_event("memory", "minus", socket) do
+    update_memory(socket, socket.assigns.memory - display_to_float(socket))
   end
 
   def handle_event("memory", "recall", socket) do
     {:noreply, assign(socket, display: socket.assigns.memory, clear_display_on_next_digit: true )}
   end
 
-  def handle_event("memory", "store", socket) do
-    {:noreply, assign(socket, memory: display_as_float(socket), clear_display_on_next_digit: true )}
-  end
-
-  def handle_event("memory", "add", socket) do
-    {:noreply, assign(socket, memory: socket.assigns.memory + display_as_float(socket), clear_display_on_next_digit: true )}
-  end
-
-  def handle_event("memory", "minus", socket) do
-    {:noreply, assign(socket, memory: socket.assigns.memory - display_as_float(socket), clear_display_on_next_digit: true )}
-  end
-
   # Private
-  defp display_as_float(socket) do
+  defp display_to_float(socket) do
     {display, _} = Float.parse("#{socket.assigns.display}")
     display
   end
@@ -97,6 +92,17 @@ defmodule DemoWeb.CalcLive.Index do
 
   defp peform_pending_operation(socket) do
     {accumulator, _} = Float.parse("#{socket.assigns.value}")
-    {:ok, _new_value} = Abacus.eval("#{accumulator} #{socket.assigns.pending_operator} #{display_as_float(socket)}")
+    {:ok, _new_value} = Abacus.eval("#{accumulator} #{socket.assigns.pending_operator} #{display_to_float(socket)}")
+  end
+
+  defp update_display(digit, socket = %{assigns: %{clear_display_on_next_digit: true}} ) do
+    {:noreply, assign(socket, display: digit, clear_display_on_next_digit: false)}
+  end
+  defp update_display(digit, socket ) do
+    {:noreply, assign(socket, display: "#{socket.assigns.display}#{digit}")}
+  end
+
+  defp update_memory(socket, new_memory) do
+    {:noreply, assign(socket, memory: new_memory, clear_display_on_next_digit: true )}
   end
 end
